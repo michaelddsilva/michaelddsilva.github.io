ffa59f1e98f871f3fb59c06252d06c46c078b024
michaelddsilva <michael.dsilva.343@gmail.com>
2019-04-25 22:57:04 -0400

    Update

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload=function onloadfunction(){
+    copyrightYear();
+}
+
+function copyrightYear(){
+    var year = new Date().getFullYear();
+
+    document.querySelector("footer p").innerHTML = "Copyright <i class='far fa-copyright'></i> " + year + " Michael D'Silva";
+
+}
