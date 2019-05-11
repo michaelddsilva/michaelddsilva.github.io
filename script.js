9b06827de96bd8b8f8005e7937ceb78c1809e7b5
michaelddsilva <michael.dsilva.343@gmail.com>
2019-05-11 16:44:12 -0400

    Update

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ window.onload = function onloadfunction() {
 function copyrightYear() {
     var year = new Date().getFullYear();
 
-    document.querySelector("footer p").innerHTML = "Copyright <i class='far fa-copyright'></i> " + year + " Michael D'Silva";
+    document.querySelector("footer p").innerHTML = "Almost Copyright <i class='far fa-copyright'></i> " + year + " Michael D'Silva";
 
 }
