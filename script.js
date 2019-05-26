8759ba6b6b54f3933005eb313d49ec8a92e40fa9
michaelddsilva <michael.dsilva.343@gmail.com>
2019-05-26 19:18:26 -0400

    Update

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+var scrollEventHandler = function()
+{
+  window.scroll(0, window.pageYOffset)
+}
+
+window.addEventListener("scroll", scrollEventHandler, false);
