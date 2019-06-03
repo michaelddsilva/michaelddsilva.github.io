cd1c6e5ce66fde44a24ead308ddb471ce97fc9cf
michaelddsilva <michael.dsilva.343@gmail.com>
2019-06-02 21:04:46 -0400

    Updated index.html, styles.css, and script.js

--- script.js
+++ script.js
@@ -43,3 +43,14 @@ function typeMyName() {
   }
 }
 
+function swapCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
